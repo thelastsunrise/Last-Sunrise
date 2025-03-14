35f7d5a4f54fae98c9e21527f3ffe6c67ee4361c
Cat <cat@lastsunrise.art>
2025-03-14 18:02:00 -0400

    Rebuild website

--- src/pages/changelog-rss.xml.js
+++ src/pages/changelog-rss.xml.js
@@ -8,7 +8,7 @@ return rss({
     items: [
     {
         title: "3.0.4",
-        description: "Added a button to return to the top of any page, and fixed the sizing of images in blog posts...again.",
+        description: "Added a button to return to the top of any page, and fixed the sizing of images in blog posts...again. Also added a button to the Persona 3 fanlisting!",
         pubDate: "2025-03-14",
     },
     {
