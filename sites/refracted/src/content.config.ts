8419dfb26e6812983069f8c30d42dcef31eca205
Cat <cat@lastsunrise.art>
2025-03-25 18:36:14 -0400

    Create content collection for posts

---
+++ sites/refracted/src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+import { glob, file } from "astro/loaders";
+
+const articles = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/pages" }),
+    schema: z.object({
+        title: z.string(),
+        section: z.number(),
+        id: z.number(),
+        description: z.string(),
+    })
+});
+
+export const collections = { articles };
