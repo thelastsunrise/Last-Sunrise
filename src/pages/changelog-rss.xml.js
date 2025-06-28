2e7abd1d1a290e131b3926c1f3aff67e3dc8214e
Cat <cat@lastsunrise.art>
2025-06-27 21:02:52 -0400

    Rebuild site

--- src/pages/changelog-rss.xml.js
+++ src/pages/changelog-rss.xml.js
@@ -7,6 +7,12 @@ return rss({
     site: context.site,
     items: [
     {
+        title: "3.1.1",
+        description: "Fixed up the half broken Sims page CSS and published a new part of the legacy saga!",
+        pubDate: "2025-06-27",
+        link: "https://lastsunrise.art/shrines/sims/basilio-g2-3/"
+    },
+    {
         title: "3.1.0",
         description: "Made real actual CSS for the Sims shrine, which means we're onto a new major site version :-)",
         pubDate: "2025-06-26",
