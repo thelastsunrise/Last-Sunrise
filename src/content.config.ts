6a219c4c6c6d23963402c7aa7ac4c57af5b962b4
Cat <cat@lastsunrise.art>
2025-06-28 16:31:20 -0400

    Update content config for cast list

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@
 import { defineCollection, z } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const blog = defineCollection({
@@ -14,6 +14,7 @@ const blog = defineCollection({
     })
 });
 
+// SIMS SHRINE CONTENT COLLECTIONS
 const sims = defineCollection({
     loader: glob({pattern: "*.mdx", base: "src/content/sims"}),
     schema: z.object({
@@ -23,5 +24,26 @@ const sims = defineCollection({
     })
 });
 
+const simsCast = defineCollection({
+    loader: file("src/content/sims/cast.json"),
+    schema: z.object({
+        familyid: z.string(),
+            game: z.string(),
+            generation: z.array(
+                z.object({
+                    number: z.number(),
+                    cast: z.array(
+                        z.object({
+                            name: z.string(),
+                            gender: z.string(),
+                            pronouns: z.string(),
+                            role: z.string()
+                        })
+                    )
+                })
+        )
+    })
+});
+
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { sims, blog };
+export const collections = { blog, sims, simsCast };
