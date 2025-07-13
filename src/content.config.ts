1689476b926a553001771d477cadca25689089eb
Cat <cat@lastsunrise.art>
2025-07-12 20:59:59 -0400

    Update content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -49,6 +49,7 @@ const simsCast = defineCollection({
                                     teenagesrc: z.string().optional(),
                                     youngadultsrc: z.string().optional(),
                                     matureadultsrc: z.string().optional(),
+                                    occultsrc: z.string().optional(),
                                     eldersrc: z.string().optional()
                                 })
                             ),
